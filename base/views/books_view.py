bf377f12fa6c91e71ee6512b5e4f25cd1b6aa091
liorosenman <lros209@gmail.com>
2024-10-22 14:29:06 +0300

    commit-11

--- base/views/books_view.py
+++ base/views/books_view.py
@@ -29,22 +29,20 @@ class BookViewSet(viewsets.ModelViewSet):
         # parameters = extract_parameters_from_request(request)
         proc_name = 'add_book'
         print(request.data.get('borrow_time'))
-        param_list = [
-            request.data.get('name'),
-            request.data.get('author'),
-            int(request.data.get('year_published')),
-            int(request.data.get('borrow_time')),
-            request.data.get('filename'),
-            request.data.get('status')
-        ]
+        name = request.data.get('name'),
+        author = request.data.get('author'),
+        year_published = int(request.data.get('year_published')),
+        borrow_time = int(request.data.get('borrow_time')),
+        filename = request.data.get('filename'),
+        status = request.data.get('status')
         # param_list = list(parameters.values())
         try:
             with connection.cursor() as cursor:
                 # cursor.callproc(proc_name, param_list)
-                cursor.execute("CALL add_book(%s, %s, %s, %s, %s, %s);", param_list)
-            return Response({"detail": "Book added successfully."}, status=status.HTTP_201_CREATED)
+                cursor.execute("CALL add_book(%s, %s, %s, %s, %s, %s);", (name, author, year_published, borrow_time, filename,status))
+            return Response({"detail": "Book added successfully."})
         except Exception as e:
-            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": str(e)})
 
 
 
